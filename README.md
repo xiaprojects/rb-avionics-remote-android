3ba5eb6029bf50fae04fc970ac8470d59001a786
xiaprojects <xiaprojects@users.noreply.github.com>
2025-10-10 21:20:55 +0200

    Full screen WebView with multiple display

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# rb-avionics-remote-android
+# RB-03 Viewer
 RB-Avionics Remote Viewer and Controller for Android
+
+### Requirements
+1. Android device shall be already connected to RB-01 or RB-03
+2. WiFi is trusted = you can open the http://192.168.10.1 with Chrome
+
+# Features
+1. Support multiple Android display
+2. On each screen the Viewer opens the website at full screen
+
+## Remote display
+- Primary display: http://192.168.10.1/RB-01
+- Secondary display: http://192.168.10.1/RB-01
---
+++ app/src/main/java/com/xiaprojects/rb/MainActivity.kt
@@ -0,0 +1,219 @@
+package com.xiaprojects.rb
+
+import android.annotation.SuppressLint
+import android.app.ActivityOptions
+import android.app.Presentation
+import android.content.Context
+import android.content.Intent
+import android.hardware.display.DisplayManager
+import android.os.Bundle
+import android.view.Display
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+
+open class FullScreenWebViewActivity : AppCompatActivity() {
+    protected lateinit var webView: WebView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Hide ActionBar
+        supportActionBar?.hide()
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.let { controller ->
+                controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                controller.systemBarsBehavior =
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    )
+        }
+
+
+
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webview)
+
+        webView.settings.apply {
+            @SuppressLint("SetJavaScriptEnabled")
+            javaScriptEnabled = true
+            domStorageEnabled = true
+
+            // Enable caching
+            //cacheMode = WebSettings.LOAD_DEFAULT
+            cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
+
+            // Optional: improve performance
+            setRenderPriority(WebSettings.RenderPriority.HIGH)
+            loadsImagesAutomatically = true
+            setSupportZoom(true)
+
+            // Optional: enable local file access if needed
+            allowFileAccess = true
+            allowContentAccess = true
+        }
+
+        /*
+        webView.clearCache(true)
+        webView.clearHistory()
+        webView.clearFormData()
+        */
+        webView.webViewClient = WebViewClient()
+    }
+
+    override fun onBackPressed() {
+        if (this::webView.isInitialized && webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    )
+        }
+    }
+}
+
+class MainActivity : FullScreenWebViewActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //
+        super.onCreate(savedInstanceState)
+        //
+        webView.loadUrl(resources.getString(R.string.urlMain))
+
+        //
+        val displayManager = getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+        val displays = displayManager.displays
+        for (display in displays) {
+            if (display.displayId != Display.DEFAULT_DISPLAY) {
+                val options = ActivityOptions.makeBasic()
+                options.launchDisplayId = display.displayId
+                startActivity(
+                    Intent(this@MainActivity, SecondActivity::class.java),
+                    options.toBundle()
+                )
+            }
+        }
+    }
+}
+
+
+class SecondActivity : FullScreenWebViewActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //
+        super.onCreate(savedInstanceState)
+        //
+        webView.loadUrl(resources.getString(R.string.urlSlave))
+    }
+/*
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Hide ActionBar
+        supportActionBar?.hide()
+
+
+
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.let { controller ->
+                controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                controller.systemBarsBehavior =
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    )
+        }
+
+
+
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webview)
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl(resources.getString(R.string.urlSlave))
+
+
+        val displayManager = getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+        val displays = displayManager.displays
+        for (display in displays) {
+            if (display.displayId != Display.DEFAULT_DISPLAY)             {
+                /*
+                val presentation = SecondaryScreen(this, display)
+                presentation.show()
+                 */
+            }
+
+        }
+    }
+
+    override fun onBackPressed() {
+        if (this::webView.isInitialized && webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    )
+        }
+    }
+*/
+}
+
