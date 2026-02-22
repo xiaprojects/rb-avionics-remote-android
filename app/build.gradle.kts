55f8db35c4440763d56a407b32c489ebd5c7d8a1
Brielli <alphagamma.thetrueone@gmail.com>
2026-02-22 14:35:31 +0100

    updated to v1.3.0 (7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.xiaprojects.rb"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.2.0"
+        versionCode = 7
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
