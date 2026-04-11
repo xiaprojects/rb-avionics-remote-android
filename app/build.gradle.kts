68394f4b72b40fcef5963cc4345024a58ff8177b
Brielli <alphagamma.thetrueone@gmail.com>
2026-04-11 17:21:33 +0200

    update to v1.4.0 (8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.xiaprojects.rb"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.3.0"
+        versionCode = 8
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
